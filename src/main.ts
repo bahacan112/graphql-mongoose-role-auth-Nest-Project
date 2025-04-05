e63d47a601c7ea040035ec6bf77e8de614baf6ca
Bahattin ZENBIL <bzenbil19@gmail.com>
2025-04-05 04:11:02 +0300

    origin düzeltildi

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import { graphqlUploadExpress } from 'graphql-upload-ts';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: 'https://frontend-operation.vercel.app/',
     credentials: true,
   });
   // ✅ Upload middleware ÖNCE
